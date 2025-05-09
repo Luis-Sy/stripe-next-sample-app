64f7c31cea5549fa51015fa88b8a14ad864debbc
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2025-05-08 23:46:07 -0600

    mobile css adjustments

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <>
             <div id="home">
+                <h1>Home Page</h1>
                 <div id="topBanner"></div>
             </div>
         </>
