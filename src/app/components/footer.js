85a316a6a00800dfb107c9c2fbd5181bd64ae4c5
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2025-05-25 02:08:14 -0600

    added sample banner to home page

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
         <footer id="footer">
             <h2>FakeStore</h2>
             <h3>Created by: Luis Marlou Sy</h3>
-            <h3>Contact: luismarlousy@gmail.com</h3>
+            <h3>Contact: <a href="mailto:luismarlousy@gmail.com">luismarlousy@gmail.com</a></h3>
         </footer>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
 "use client"
 
+import Image from "next/image";
+
 export default function Home() {
     return (
         <>
             <div id="home">
                 <h1>Home Page</h1>
-                <div id="topBanner"></div>
+                <div id="topBanner" style={{width: "100%"}}>
+                    <div style={{ position: "relative", width: "100%", height: "500px"}}>
+                        <Image
+                            src="/images/banner.jpg"
+                            alt="Top Banner Image"
+                            fill
+                            style={{ objectFit: "cover", objectPosition: "center" }}
+                        />
+                    </div>
+                </div>
             </div>
         </>
     )
